088e485ffe46c07a4f3f419f52c296d713c25720
paramashiva123 <kambalaparamashiva123@gmail.com>
2025-04-10 11:23:30 +0530

    project structure ready,venv set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Paramashiva",
+    author_email="kambalaparamashiva123@gmail.com",
+    packages=find_packages()
+)
